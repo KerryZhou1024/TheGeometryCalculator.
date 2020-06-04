af4896c7025ef8eadfbdef9b9290d5d632b19b9e
Kerry Zhou <cosmosedition@gmail.com>
2020-06-04 15:31:32 -0400

    Initial Commit

---
+++ Geometry calculator/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Geometry calculator
+//
+//  Created by Kerry Zhou on 6/4/20.
+//  Copyright © 2020 Kerry Zhou. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Geometry calculatorTests/Geometry_calculatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  Geometry_calculatorTests.swift
+//  Geometry calculatorTests
+//
+//  Created by Kerry Zhou on 6/4/20.
+//  Copyright © 2020 Kerry Zhou. All rights reserved.
+//
+
+import XCTest
+@testable import Geometry_calculator
+
+class Geometry_calculatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
